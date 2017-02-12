99fc9186c2aaf36488f395720aedd9e5103babc9
vhoyer <vhoyer@live.com>
2017-02-12 15:37:36 -0200

    Ep5: Colour & Rendering Optimisation
    Signed-off-by: vhoyer <vhoyer@live.com>

--- src/Game.java
+++ src/Game.java
@@ -11,6 +11,7 @@ import java.awt.image.DataBufferInt;
 import javax.swing.JFrame;
 import io.github.vhoyer.game.gfx.SpriteSheet;
 import io.github.vhoyer.game.gfx.Screen;
+import io.github.vhoyer.game.gfx.Colours;
 
 public class Game extends Canvas implements Runnable {
 	public static final long serialVersionUID = 1L;
@@ -27,6 +28,7 @@ public class Game extends Canvas implements Runnable {
 
 	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 	private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
+	public int[] colours = new int[6 * 6 * 6]; //216
 
 	private Screen screen;
 	public InputHandler input;
@@ -50,6 +52,19 @@ public class Game extends Canvas implements Runnable {
 	}
 
 	public void init(){
+		int index = 0;
+		for(int r = 0; r < 6; r++){
+			for(int g = 0; g < 6; g++){
+				for(int b = 0; b < 6; b++){
+					int rr = (r * 255/5);
+					int gg = (g * 255/5);
+					int bb = (b * 255/5);
+
+					colours[index++] = rr << 16 | gg << 8 | bb;
+				}
+			}
+		}
+
 		screen = new Screen(WIDTH, HEIGHT, new SpriteSheet("/sprite_sheet.png"));
 		input = new InputHandler(this);
 	}
@@ -130,11 +145,22 @@ public class Game extends Canvas implements Runnable {
 			return;
 		}
 
-		screen.render(pixels, 0, WIDTH);
+		for(int y = 0; y < 32; y++){
+			for(int x = 0; x < 32; x++){
+				screen.render(x << 3, y << 3, 0, Colours.get(555, 500, 050, 005));
+			}
+		}
+
+		for(int y = 0; y < screen.height; y++){
+			for(int x = 0; x < screen.width; x++){
+				int colourCode = screen.pixels[x+y * screen.width];
+				if(colourCode < 255){
+					pixels[x+y * WIDTH] = colours[colourCode];
+				}
+			}
+		}
 
 		Graphics g = bs.getDrawGraphics();
-		
-		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
 
 		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
 
--- src/SpriteSheet.java
+++ src/SpriteSheet.java
@@ -31,9 +31,5 @@ public class SpriteSheet {
 		for(int i = 0; i < pixels.length; i++){
 			pixels[i] = (pixels[i] & 0xff) / 64;
 		}
-
-		for(int i = 0; i < 8; i++){
-			System.out.println(pixels[i]);
-		}
 	}
 }
